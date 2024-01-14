6cd065dd1abeb3e204187d0b566456430744e33c
auqid <auqidirfan100@gmail.com>
2024-01-14 16:26:36 +0530

    Express server setup, fetching products from backend using axios TODO:setup a database

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,22 @@
 import { useParams } from "react-router-dom";
 import React from 'react'
-import products from "../products";
+import { useEffect,useState } from "react";
 import {Row,Col,Image,ListGroup,Card,Button,} from 'react-bootstrap'
 import { Link } from "react-router-dom";
 import Rating from "../components/Rating";
+import axios from 'axios'
 const ProductScreen = () => {
+    const [product, setProduct]= useState({});
+    
     const {id:productId} = useParams()
-    const product = products.find((p)=>p._id=== productId)
+    useEffect(()=>{
+        const fetchProduct = async()=>{
+            const {data} = await axios.get(`/api/products/${productId}`)
+            setProduct(data)
+        }
+        fetchProduct()
+    },[productId])
+    
   
     return (
     <>
