5d500f7de81fcf03fffb3ea11a62b7c2b96c9ef8
auqid <38567390+auqid@users.noreply.github.com>
2024-01-16 19:57:14 +0530

    Added error handling and controller file for routes.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,24 +1,13 @@
 import  express  from "express";
-import Product from '../models/productModel.js'
-import asynchHandler from "../middleware/asyncHandler.js";
-
-
 const router = express.Router();
+import { getProducts, getProductById } from "../controllers/productController.js";
 
 
 
-router.get('/',asynchHandler (async (req,res)=>{
-    const products = await Product.find({});
-    res.json(products)
-}))
 
-router.get('/:id',asynchHandler(async(req,res)=>{
-    const product = await Product.findById(req.params.id);
-    if(product){
-        res.json(product)
-    }
-    res.status(404).json({message:'Product not found'})
-    
-}))
+
+
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById)
 
 export default router;
