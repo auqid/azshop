fa07bd898848792294d7ceb1e72725c27fb41444
auqid <auqidirfan100@gmail.com>
2024-01-24 14:58:42 +0530

    Added logout functionality- Clearing jwt  cookies on logout.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,11 +43,17 @@ const registerUser= asyncHandler(async(req,res)=>{
     res.send('Register User')
     });
 
-    // @desc Logout /clear cookie
+// @desc Logout /clear cookie
 // @route POST/api/logout
 // @access Private 
 const logoutUser = asyncHandler(async(req,res)=>{
-    res.send('Logout User')
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+    
+    
+    res.status(200).json({message: 'Logged out successfully'})
     });
 
 // @desc Get user profile
